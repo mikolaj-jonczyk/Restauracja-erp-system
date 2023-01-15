72e736066b2f754597dc6466fa57a3dbcc287011
Mikołaj Jonczyk <mikolaj.jon@gmail.com>
2023-01-15 10:43:50 +0100

    Merge branch 'main' of https://github.com/mikolaj-jonczyk/Restauracja-erp-system
    # Conflicts:
    #	Restauracja-erp-system/src/controllers/work.controller.ts

--- Restauracja-erp-system/src/test/test.spec.ts
+++ Restauracja-erp-system/src/test/test.spec.ts
@@ -2,7 +2,7 @@ import { Work } from '../models/tasks/work.entity';
 import { WorkRepository } from '../repositories/work.repository';
 import { WorkController } from '../controllers/work.controller';
 import { WorkService } from '../services/work.service';
-  
+
 describe('WorkController', () => {
   let workController: WorkController;
   let workService: WorkService;
@@ -11,11 +11,13 @@ describe('WorkController', () => {
     workService = new WorkService(new WorkRepository());
     workController = new WorkController(workService);
   });
-//".*\\.spec\\.ts$",
+  //".*\\.spec\\.ts$",
   describe('getTaskById', () => {
     it('should return an array of cats', async () => {
       const result = new Work();
-      jest.spyOn(workService, 'getTaskById').mockImplementation(async () =>  await result);
+      jest
+        .spyOn(workService, 'getTaskById')
+        .mockImplementation(async () => await result);
 
       expect(await workController.getTaskById('1')).toBe(result);
     });
